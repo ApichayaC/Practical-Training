adb30d6dedc2bca833fc107e6d917bf977d7a739
ApichayaC <fay.tbl@gmail.com>
2022-05-18 23:24:07 +0700

    create assignment2

--- Assignment/Assignment1/app.ts
+++ Assignment/Assignment1/app.ts
@@ -38,21 +38,60 @@ const fetchAPI = () => {
         console.log(err);
     };
 }
-fetchAPI()
+//fetchAPI()
 
 //2
-function charInput(){
+function charInput() {
     fetch('https://api1.binance.com/api/v3/klines?interval=1h&symbol=BTCUSDT')
-    .then(res=>res.json())
-    .then(data=>{
-        const openTime= data[0][0] 
-        const date = new Date(openTime);
-        const hours = date.getHours();
-        const minutes = "0"+date.getMinutes();
-        const formattedTime = date +' '+ hours+':'+minutes as string
-        console.log(formattedTime)
-      
-    })
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < data.length;) {
+                let j = 0;
+                const openTime = data[i][j]
+                const date = new Date(openTime);
+                const hours = date.getHours();
+                const minutes = "0" + date.getMinutes();
+                //const formattedTime:string = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() + ' ' + hours + ':' + minutes as string
+                //console.log(formattedTime)
+                const formattedTime: string = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${hours}:${minutes}`
+                const newObj = {
+                    formattedTime: formattedTime,
+                    time: {
+                        open: data[i][j + 1],
+                        high: data[i][j + 2],
+                        low: data[i][j + 3],
+                        close: data[i][j + 4]
+                    }
+                }
+                console.log(Object.values(newObj))
+                i++;
+            }
+        })
 }
 //charInput();
+
+//3
+const orderBook = async () => {
+    function calculateOutputAmount(usdtAmount: string) {
+        fetch('https://api1.binance.com/api/v3/depth?symbol=BTCUSDT')
+            .then(res => res.json())
+            .then((data) => {
+                const bids = data.bids
+                const test: string = bids[0].find((usdt: string) => {
+                    for (let i = 0 ; i < bids.length;) {
+                        if (usdt[0] == usdtAmount) {
+                            return console.log(usdt[0][1])
+                        }
+                        else {
+                            //console.log(usdt.length)
+                        }
+                        i++;
+                    }
+                })
+            })
+    }
+    console.log(calculateOutputAmount('29149.67000000'))
+
+}
+orderBook()
 
